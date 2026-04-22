1558072e0501ff0461abe864f02929b79c953a75
Spike <spike@volley.local>
2026-04-22 23:13:58 +0000

    Config: switch to port 8080

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 // API utility functions
 // Update this to match your backend URL
 
-const API_URL = 'http://187.124.58.56:5000/api';
+const API_URL = 'http://187.124.58.56:8080/api';
 
 // Get current user ID from device (simple implementation)
 export const getOrCreateUser = async (deviceId) => {
